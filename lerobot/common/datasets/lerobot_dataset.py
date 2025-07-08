9c8d221bcdf182aacddbf05f6620b3e18fbf9c64
CUHKWilliam <812666178@qq.com>
2025-07-08 18:56:17 +0000

    update

--- lerobot/common/datasets/lerobot_dataset.py
+++ lerobot/common/datasets/lerobot_dataset.py
@@ -732,15 +732,27 @@ class LeRobotDataset(torch.utils.data.Dataset):
             for key, val in query_result.items():
                 item[key] = val
         if len(self.meta.video_keys) > 0:
-            current_ts = item["timestamp"].item()
-            next_timestamps = self._get_query_timestamps(current_ts + 0.5, query_indices) ## TODO: set next timestamp delta time
+            ts = item["timestamp"].item()
+            end_ts = ts + 1/self.fps * float(len(self.delta_indices['action']))
+            next_timestamps = self._get_query_timestamps(end_ts, query_indices) ## TODO: set next timestamp delta time
             query_timestamps = self._get_query_timestamps(current_ts, query_indices)
             video_frames = self._query_videos(query_timestamps, ep_idx)
             next_video_frames = self._query_videos(next_timestamps, ep_idx)
             next_video_frames2 = {}
             for k in next_video_frames.keys():
                 next_video_frames2[f"next.{k.replace('observation.', '')}"] = next_video_frames[k]
-            item = {**video_frames, **item, **next_video_frames2}
+
+        
+            current_ts = current_ts + np.random.rand() * (end_ts - ts)
+            current_timestamps = self._get_query_timestamps(current_ts, query_indices) ## TODO: set next timestamp delta time
+            query_timestamps = self._get_query_timestamps(current_ts, query_indices)
+            video_frames = self._query_videos(query_timestamps, ep_idx)
+            current_video_frames = self._query_videos(current_timestamps, ep_idx)
+            current_video_frames2 = {}
+            for k in next_video_frames.keys():
+                current_video_frames2[f"current.{k.replace('observation.', '')}"] = next_video_frames[k]
+           
+            item = {**video_frames, **item, **next_video_frames2, **current_video_frames2}
 
         if self.image_transforms is not None:
             image_keys = self.meta.camera_keys
